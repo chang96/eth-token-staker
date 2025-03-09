9c4b899356a30e66993434ff83bcbceea8d4e170
rahman Orewole <rorewole@gmail.com>
2025-03-09 15:50:14 +0100

    hardhat configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
 import type { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
-
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
 };
